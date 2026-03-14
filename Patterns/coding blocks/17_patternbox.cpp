64b28856e7cd34cd95a8d204e540285e433775ee
manasrathore3 <manasrathore93@gmail.com>
2026-03-14 16:28:02 +0530

    using mid in hollow diamond

--- Patterns/coding blocks/17_patternbox.cpp
+++ Patterns/coding blocks/17_patternbox.cpp
@@ -6,28 +6,51 @@ int main(){
     int n,mid;
     cout<<"Enter N :" ;
     cin>>n;
+
+    // mid=(n+1)/2;                             // Butterfly hollow style pattern
+
+    // for (int i = 0; i < mid; i++){
+    //     for (int j = 0; j < mid-1-i; j++){
+    //         cout<<"* ";
+    //     }
+    //     for (int j = 0; j < 2*i+1; j++){
+    //         cout<<"  ";
+    //     }
+    //     for (int j = (mid-1-i); j > 0; j--){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // for (int i = 0; i < mid-1; i++){
+    //     for (int j = 0; j < i+1; j++){
+    //         cout<<"* ";
+    //     }
+    //     for (int j = 0; j < 2*mid-3-2*i; j++){
+    //         cout<<"  ";
+    //     }
+    //     for (int j = 0; j < i+1; j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for next pattern (butterfly hollow style pattern ka reverse)
     mid=(n+1)/2;
 
     for (int i = 0; i < mid; i++){
         for (int j = 0; j < mid-1-i; j++){
-            cout<<"* ";
-        }
-        for (int j = 0; j < 2*i+1; j++){
             cout<<"  ";
         }
-        for (int j = (mid-1-i); j > 0; j--){
+        for (int j = 0; j < 2*i+1; j++){
             cout<<"* ";
         }
         cout<<endl;
     }
     for (int i = 0; i < mid-1; i++){
         for (int j = 0; j < i+1; j++){
-            cout<<"* ";
-        }
-        for (int j = 0; j < 2*mid-3-2*i; j++){
             cout<<"  ";
         }
-        for (int j = 0; j < i+1; j++){
+        for (int j = 0; j < 2*mid-3-2*i; j++){
             cout<<"* ";
         }
         cout<<endl;
